0a7b15c3e895d250f55a84b6b387b9c55ad11304
André L. C. Ferreira <alcf.2099@gmail.com>
2016-08-01 12:16:47 -0300

    new parameters

--- main.js
+++ main.js
@@ -4,25 +4,27 @@ var csvWriter = require('csv-write-stream');
 var fs = require('fs');
 var writer = csvWriter();
 
-// process.argv[2] -> email:senha
-const auth = `Basic ${new Buffer(process.argv[2]).toString('base64')}`;
-// process.argv[3] -> AAAA-MM-DD
-const timestamp = new Date(process.argv[3]).getTime();
+// node main.js jiraTeam email password initialDate
+
+const baseUrl = `https://${process.argv[2]}.atlassian.net/rest/api/latest`;
+const email = process.argv[3];
+const auth = `Basic ${new Buffer(`${process.argv[3]}:${process.argv[4]}`).toString('base64')}`;
+const timestamp = new Date(process.argv[5]).getTime();
 
 co(function *() {
-	console.log('Recuperando worklogs ids...');
+	console.log('Retrieving worklogs ids...');
 	let worklogs = yield getWorkLogs(timestamp);
-	console.log('Qtde worklogs:', worklogs.values.length);
+	console.log('Qtt worklogs:', worklogs.values.length);
 
 	console.log();
-	console.log('Recuperando lista de worklogs');
+	console.log('Retrieving worklogs list');
 	const listOfIds = worklogs.values.map(x => x.worklogId);
 	worklogList = yield getWorkLogList(listOfIds);
 
 	console.log();
-	console.log('Processando...');
+	console.log('Processing...');
 
-	const byUser = worklogList.filter(x => x.author.name === 'andre.ferreira');
+	const byUser = worklogList.filter(x => x.author.emailAddress === email);
 
 	let totalInSeconds = 0;
 
@@ -38,18 +40,18 @@ co(function *() {
 	});
 
 	writer.write([])
-	writer.write(['Total em horas', '', totalInSeconds / 60 / 60]);
+	writer.write(['Total', '', totalInSeconds / 60 / 60]);
 
 	writer.end()
 	console.log();
-	console.log('Done');
+	console.log('Done. Check file out.csv');
 
 }).catch(err => console.log('error:', err));
 
 function getWorkLogs(timestamp) {
 	return new Promise(function(resolve, reject) {
 		const options = {
-			url: `https://2gather.atlassian.net/rest/api/latest/worklog/updated?since=${timestamp}`,
+			url: `${baseUrl}/worklog/updated?since=${timestamp}`,
 			headers: { Authorization: auth }
 		};
 
@@ -64,7 +66,7 @@ function getWorkLogs(timestamp) {
 function getWorkLogList(list) {
 	return new Promise(function(resolve, reject) {
 		const options = {
-			url: `https://2gather.atlassian.net/rest/api/latest/worklog/list`,
+			url: `${baseUrl}/worklog/list`,
 			headers: { Authorization: auth, 'Content-Type': 'application/json' },
 			body: JSON.stringify({ ids: list })
 		};
